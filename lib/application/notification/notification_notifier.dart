6307c5c4b53318d9a7865b18155bc7811505a53e
sahinefe35 <efe.mate@gmail.com>
2022-10-07 19:13:09 +0300

    create mapEventsToState method

--- lib/application/notification/notification_notifier.dart
+++ lib/application/notification/notification_notifier.dart
@@ -1,10 +1,14 @@
+import 'package:awesome_notifications_with_riverpod/application/notification/notification_event.dart';
 import 'package:awesome_notifications_with_riverpod/application/notification/notification_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class NotificationNotifier extends StateNotifier<NotificationState> {
   NotificationNotifier() : super(NotificationState.empty());
 
-  void mapEventsToState(NotificationState event) {
-    //Todo: Implement the functions which are related to Notifications
+  void mapEventsToState(NotificationEvent event) {
+    event.map(
+      sendNormalNotification: (sendNormalNotificationEvent) {},
+      sendNotificationAfter3Seconds: (sendNotificationAfter3Seconds) {},
+    );
   }
 }
