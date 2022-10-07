f1663a456c0ee66e5b9c4ce7aea52bebafad412e
sahinefe35 <efe.mate@gmail.com>
2022-10-07 21:33:38 +0300

    chore: implementation for the notification buttons

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -1,12 +1,42 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:awesome_notifications_with_riverpod/presentation/routes/router.gr.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AppWidget extends StatelessWidget {
+class AppWidget extends StatefulWidget {
   const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  State<AppWidget> createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<AppWidget> {
+  @override
+  void initState() {
+    AwesomeNotifications().setListeners(
+      onActionReceivedMethod: (receivedAction) async {},
+    );
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final _appRouter = AppRouter();
+
+    AwesomeNotifications().initialize(
+        null,
+        [
+          NotificationChannel(
+            channelGroupKey: 'basic_channel_group',
+            channelKey: 'basic_channel',
+            channelName: 'Basic notifications',
+            channelDescription: 'Notification channel for basic tests',
+            defaultColor: Colors.purpleAccent,
+            ledColor: Colors.white,
+          )
+        ],
+        debug: true);
 
     return ProviderScope(
       child: MaterialApp.router(
