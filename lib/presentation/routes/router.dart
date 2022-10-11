9d5a4a541db0ba150d55a6f9af191440b275e29f
sahinefe35 <efe.mate@gmail.com>
2022-10-11 22:04:05 +0300

    chore: remove the unnecessary lines

--- lib/presentation/routes/router.dart
+++ lib/presentation/routes/router.dart
@@ -3,7 +3,6 @@ import 'package:awesome_notifications_with_riverpod/presentation/pages/notificat
 import 'package:awesome_notifications_with_riverpod/presentation/pages/orange_page/orange_page.dart';
 import 'package:awesome_notifications_with_riverpod/presentation/pages/purple_page/purple_page.dart';
 
-
 @MaterialAutoRouter(
   replaceInRouteName: 'Page,Route',
   routes: <AutoRoute>[
